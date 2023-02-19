3d4d7acd36d158d2d17b98d2c7936820f0260ad3
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-19 19:25:41 +0100

    feat: add message fetching

---
+++ internal/service/messagefetcher/fetch_messages.go
@@ -0,0 +1,23 @@
+package messagefetcher
+
+import (
+	"context"
+
+	"scribbler/internal/models"
+
+	"github.com/pkg/errors"
+)
+
+func (s *Service) FetchMessages(ctx context.Context, query models.MessageFilter) error {
+	messages, err := s.msgSource.QueryMessages(ctx, &query)
+	if err != nil {
+		return errors.Wrap(err, "fetching messages from source")
+	}
+
+	err = s.qWriter.PostMessages(ctx, messages)
+	if err != nil {
+		return errors.Wrap(err, "posting message to queue")
+	}
+
+	return nil
+}
