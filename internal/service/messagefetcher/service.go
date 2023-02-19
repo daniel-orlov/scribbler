2d8e408f318cce605eaed099623d06b5dd7295b6
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-19 19:20:54 +0100

    feat: add message fetcher service definition

---
+++ internal/service/messagefetcher/service.go
@@ -0,0 +1,28 @@
+package messagefetcher
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"scribbler/internal/models"
+)
+
+type MessageSource interface {
+	QueryMessages(ctx context.Context, filter *models.MessageFilter) ([]models.Message, error)
+}
+
+type QueueWriter interface {
+	PostMessages(ctx context.Context, messages []models.Message) error
+}
+
+type Service struct {
+	log       *zap.Logger
+	msgSource MessageSource
+	qWriter   QueueWriter
+}
+
+// NewService creates a new instance of the Message Fetcher service.
+func NewService(log *zap.Logger, msgSource MessageSource, qWriter QueueWriter) *Service {
+	return &Service{log: log, msgSource: msgSource, qWriter: qWriter}
+}
