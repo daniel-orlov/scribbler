8ae486bdfb7344f36039a8aa340c6b6cadfceda7
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-19 19:16:24 +0100

    feat: add fake twitter client

---
+++ internal/adapters/twit/client.go
@@ -0,0 +1,40 @@
+package twit
+
+import (
+	"context"
+	"math/rand"
+
+	"go.uber.org/zap"
+
+	"scribbler/cfg"
+	"scribbler/internal/models"
+)
+
+type Client struct {
+	cfg *cfg.Config
+	log *zap.Logger
+}
+
+func NewClient(log *zap.Logger) *Client {
+	return &Client{log: log}
+}
+
+// QueryMessages returns the messages that match the query.
+// Note: This implementation uses fake tweet generator for now.
+func (c *Client) QueryMessages(_ context.Context, _ *models.MessageFilter) ([]models.Message, error) {
+	fakeTweets := generateFakeTweets(rand.Intn(60))
+
+	c.log.Info("successfully fetched messages", zap.Int("count", len(fakeTweets)))
+
+	return fakeTweets, nil
+}
+
+func generateFakeTweets(count int) []models.Message {
+	tweets := make([]models.Message, count, 0)
+
+	for i := 0; i < count; i++ {
+		tweets = append(tweets, models.NewFakeMessage())
+	}
+
+	return tweets
+}
