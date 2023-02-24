a624011eb2ebe867879857bd4c930c5929c2bef4
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-24 13:49:57 +0100

    feat: refactor server creation

--- internal/transport/server.go
+++ internal/transport/server.go
@@ -12,6 +12,11 @@ import (
 	"scribbler/cfg"
 )
 
+// Handler is an interface for abstracting handler initialization from a server.
+type Handler interface {
+	Init(rg *gin.RouterGroup, middlewares []func() gin.HandlerFunc)
+}
+
 type Server struct {
 	logger *zap.Logger
 	cfg    *cfg.Config
@@ -22,11 +27,11 @@ type Server struct {
 const EightMb = 1 << 20
 
 // NewServer creates an HTTP server, adds logging and sets timeouts.
-func NewServer(logger *zap.Logger, cfg *cfg.Config, engine *gin.Engine) *Server {
+func NewServer(logger *zap.Logger, cfg *cfg.Config) *Server {
 	server := &Server{
 		logger: logger,
 		cfg:    cfg,
-		engine: engine,
+		engine: gin.New(),
 	}
 
 	server.init()
@@ -35,6 +40,8 @@ func NewServer(logger *zap.Logger, cfg *cfg.Config, engine *gin.Engine) *Server 
 }
 
 func (s *Server) init() *http.Server {
+	gin.SetMode(s.cfg.GinMode)
+
 	// Adding logging and recovery middleware.
 	// These are the global middlewares, which will be called for every request.
 	s.engine.Use(ginzap.Ginzap(s.logger, time.RFC3339, true))
@@ -65,10 +72,10 @@ func (s *Server) Run() error {
 	return s.engine.Run(fmt.Sprint(":", s.cfg.GinPort))
 }
 
-func (s *Server) RegisterHandlers(handlers ...Handler) {
+func (s *Server) RegisterHandlers(handlers map[Handler][]func() gin.HandlerFunc) {
 	root := s.engine.Group("")
 
-	for i := range handlers {
-		handlers[i].Init(root, nil)
+	for handler, mids := range handlers {
+		handler.Init(root, mids)
 	}
 }
